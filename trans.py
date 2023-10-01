855ca8355ec6e461109ef7619604a85534581f55
Hilary  Juma <hilary@griffinkenya.com>
2023-10-01 12:40:36 +0100

    Hello

---
+++ trans.py
@@ -0,0 +1,25 @@
+# Start by making sure the `assemblyai` package is installed.
+# If not, you can install it by running the following command:
+# pip install -U assemblyai
+#
+# Note: Some macOS users might need to use `pip3` instead of `pip`.
+
+import assemblyai as aai
+
+# replace with your API token
+aai.settings.api_key = f"7f8ee129270041dfb0f1e0bf474d26c7"
+
+# URL of the file to transcribe
+FILE_URL = "https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
+
+config = aai.TranscriptionConfig(speaker_labels=True)
+
+transcriber = aai.Transcriber()
+transcript = transcriber.transcribe(
+  FILE_URL,
+  config=config
+)
+
+for utterance in transcript.utterances:
+  print(f"Speaker {utterance.speaker}: {utterance.text}")
+
